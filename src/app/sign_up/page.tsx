0b3d73f0486c3a688beb4bc5b4f9d462ea79e942
Divine Quansah <divquan@gmail.com>
2023-11-08 00:55:13 +0000

    added: sign up page

---
+++ src/app/sign_up/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SignUpForm from "./components/signupForm";
+
+const page = () => {
+  return (
+    <div className="w-full">
+      <div className="flex flex-wrap gap-12 bg-[#ba8108] text-white p-9 items-center justify-center mt-24">
+        <div className="flex flex-col items-center justify-center gap-5">
+          <h1 className="text-4xl font-medium ">
+            Sign Up With The African Bully Registry
+          </h1>
+          <p>Join the Global Community of Bulldog Owners today, at no cost</p>
+        </div>
+        <div className="flex ">
+          <SignUpForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
